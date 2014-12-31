3e588128d03f457ac4a61474140e801d4e1cfd55
jaykumar-globalnorthstar <jay.kumar@globalnorthstar.com>
2014-12-31 21:04:36 +0500

    1st chapter

---
+++ src/sibisoft/Dollar.java
@@ -0,0 +1,15 @@
+package sibisoft;
+
+public class Dollar {
+
+	public int amount;
+
+	Dollar(int amount) {
+		this.amount= amount;
+	}
+
+	void times(int multiplier) {
+		amount*= multiplier;
+	}
+
+}
---
+++ src/sibisoft/MoneyTestCases.java
@@ -0,0 +1,14 @@
+package sibisoft;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoneyTestCases {
+
+	@Test public void testMultiplication() {
+		Dollar five= new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+		}
+}
