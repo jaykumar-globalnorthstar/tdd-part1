a48139ed8f255d64a6107c26ead5bc34afd971f6
jaykumar-globalnorthstar <jay.kumar@globalnorthstar.com>
2015-01-01 12:06:32 +0500

    3rd chapter

--- src/sibisoft/MoneyTestCases.java
+++ src/sibisoft/MoneyTestCases.java
@@ -14,4 +14,9 @@ public class MoneyTestCases {
 		product= five.times(3);
 		assertEquals(15, product.amount);
 	}
+
+	@Test public void testEquality() {
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+		assertFalse(new Dollar(5).equals(new Dollar(6)));
+	}
 }
