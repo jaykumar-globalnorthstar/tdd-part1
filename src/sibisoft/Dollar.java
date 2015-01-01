4dc7c93fe3145162ce984a03193e46acd9862304
jaykumar-globalnorthstar <jay.kumar@globalnorthstar.com>
2015-01-01 20:34:55 +0500

    chapter 9

--- src/sibisoft/Dollar.java
+++ src/sibisoft/Dollar.java
@@ -3,11 +3,14 @@ package sibisoft;
 
 public class Dollar extends Money{
 
-	Dollar(int amount) {
-		this.amount= amount;
+	private String currency;
+
+	Dollar(int amount, String currency) {
+		super(amount, currency);
 	}
 
 	Money times(int multiplier) {
-		return new Dollar(amount * multiplier);
+		return Money.dollar(amount * multiplier);
 	}
+
 }
--- src/sibisoft/MoneyTestCases.java
+++ src/sibisoft/MoneyTestCases.java
@@ -17,14 +17,19 @@ public class MoneyTestCases {
 	@Test public void testEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-		assertTrue(new Franc(5).equals(new Franc(5)));
-		assertFalse(new Franc(5).equals(new Franc(6)));
-		assertFalse(new Franc(5).equals(Money.dollar(5)));
+		assertTrue(new Franc(5,"CHF").equals(new Franc(5,"CHF")));
+		assertFalse(new Franc(5,"CHF").equals(new Franc(6,"CHF")));
+		assertFalse(new Franc(5,"CHF").equals(Money.dollar(5)));
 	}
 
 	@Test public void testFrancMultiplication() {
 		Money five = Money.franc(5);
 		assertEquals(Money.franc(10), five.times(2));
 		assertEquals(Money.franc(15), five.times(3));
+	}
+
+	@Test public void testCurrency() {
+		assertEquals("USD", Money.dollar(1).currency());
+		assertEquals("CHF", Money.franc(1).currency());
 	}
 }
