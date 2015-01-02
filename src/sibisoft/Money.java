5f4e7174a9d232a310b9ccc4c56b39e11b116e99
jaykumar-globalnorthstar <jay.kumar@globalnorthstar.com>
2015-01-02 11:41:56 +0500

    Chapter 12

--- src/sibisoft/Money.java
+++ src/sibisoft/Money.java
@@ -1,6 +1,6 @@
 package sibisoft;
 
-public class Money {
+public class Money implements Expression {
 
 	protected int amount;
 	protected String currency;
@@ -33,6 +33,10 @@ public class Money {
 
 	public String toString() {
 		return amount + " " + currency;
+	}
+	
+	public Expression plus(Money addend) {
+		return new Money(amount+addend.amount,currency);
 	}
 
 }
