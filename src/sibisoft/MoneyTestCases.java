746f7504953085a9e655d138569105d41681f1b7
jaykumar-globalnorthstar <jay.kumar@globalnorthstar.com>
2015-01-02 11:24:27 +0500

    Chapter 11

--- src/sibisoft/MoneyTestCases.java
+++ src/sibisoft/MoneyTestCases.java
@@ -17,9 +17,7 @@ public class MoneyTestCases {
 	@Test public void testEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-		assertTrue(new Franc(5,"CHF").equals(new Franc(5,"CHF")));
-		assertFalse(new Franc(5,"CHF").equals(new Franc(6,"CHF")));
-		assertFalse(new Franc(5,"CHF").equals(Money.dollar(5)));
+		assertFalse(Money.franc(5).equals(Money.dollar(5)));
 	}
 
 	@Test public void testFrancMultiplication() {
@@ -33,7 +31,4 @@ public class MoneyTestCases {
 		assertEquals("CHF", Money.franc(1).currency());
 	}
 	
-	@Test public void testDifferentClassEquality() {
-		assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
-		}
 }
